c1c8ce9da04caee199a0ea832df37aa76d457358
User Freely-tech-std <freely-tech-std@www3865.sakura.ne.jp>
2019-02-03 00:56:39 +0900

    [add] 記事個別ページの作成
    記事個別ページのためにVCに変更を加えた
    トップのタイトルが表示されない製造バグも合わせて修正

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -10,3 +10,19 @@ class PostsController extends AppController
 		//投稿を全件取得して変数にセット
 		$this->set('posts', $this->Post->find('all'));
 	}
+
+	public function view($id = null)
+	{
+		if(!$id)
+		{
+			throw new NotFoundException(__('投稿が見つかりません'));
+		}
+
+		$post = $this->Post->findById($id);
+		if(!$id)
+		{
+			throw new NotFoundException(__('投稿が見つかりません'));
+		}
+		$this->set('post', $post);
+	}
+}
