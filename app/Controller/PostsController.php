4419debd4ae81a20c81daeb4cb68baf1c754e153
User Freely-tech-std <freely-tech-std@www3865.sakura.ne.jp>
2019-02-05 00:32:00 +0900

    [add] 投稿の削除機能
    投稿の削除機能追加

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -5,6 +5,7 @@ class PostsController extends AppController
 	//使用ヘルパー
 	public $helpers = array('Html', 'Form', 'Flash');
 	public $components = array('Flash');
+
 	public function index()
 	{
 		//投稿を全件取得して変数にセット
@@ -34,13 +35,36 @@ class PostsController extends AppController
 			//モデルの状態リセット
 			$this->Post->create();
 			//データ挿入
-			if($this->Post->save($this->request->data))
+			$save_res = $this->Post->save($this->request->data);
+			if($save_res)
 			{
 				$this->Flash->success(__('投稿に成功しました'));
 				return $this->redirect(array('controller'=>'Posts','action'=>'index'));
 			}
 			$this->Flash->error(__('投稿に失敗しました'));
 		}
+	}
+
+	public function delete($id)
+	{
+		//getアクセスは弾く
+		if($this->request->is('get'))
+		{
+			throw new MethodNotAllowedException();
+		}
+
+		//投稿削除
+		$delete_res = $this->Post->delete($id);
+		if($delete_res)
+		{
+			$this->Flash->success(__('投稿を削除しました'));
+		}
+		else
+		{
+			$this->Flash->error(__('投稿の削除失敗'));
+		}
+
+		return $this->redirect(array('action'=>'index'));
 	}
 
 }
--- app/View/Posts/index.ctp
+++ app/View/Posts/index.ctp
@@ -4,9 +4,18 @@
 	<?php
 	//ループで記事出力
 	foreach($posts as $post): ?>
-	<tr><td><?php echo $post['Post']['id']; ?></td></tr>
-	<tr><td><?php echo $this->Html->link($post['Post']['title'], array('controller'=>'posts', 'action'=>'view', $post['Post']['id'])); ?></td></tr>
-	<tr><td><?php echo $post['Post']['created']; ?></td></tr>
+	<tr><td><?php echo $post['Post']['id']; ?></td><td><?php echo $this->Html->link($post['Post']['title'], array('controller'=>'posts', 'action'=>'view', $post['Post']['id'])); ?></td><td><?php echo $post['Post']['created']; ?></td></tr>
+	<tr>
+		<td>
+			<?php
+			echo $this->Form->postLink(
+			'削除',
+			array('action' => 'delete', $post['Post']['id']),
+			array('confirm' => 'Are you sure?')
+			);
+			?>
+		</td>
+	</tr>
 	<?php endforeach; ?>
 </table>
 
